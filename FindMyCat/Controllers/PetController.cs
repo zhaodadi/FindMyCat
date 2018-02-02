287c544d7b4bd40cc40994868e97448ba7a9c0a4
Dadi Zhao <zhaodadi@gmail.com>
2018-02-02 12:28:34 +1100

    Based Project Setup

---
+++ FindMyCat/Controllers/PetController.cs
@@ -0,0 +1,26 @@
+﻿using FindMyCat.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FindMyCat.Controllers
+{
+	public class PetController : Controller
+	{
+		private readonly IPetService _petService;
+
+		public PetController(IPetService petService)
+		{
+			_petService = petService;
+		}
+
+		// GET: Pet
+		public ActionResult Index()
+		{
+			_petService.DoNothing();
+			return View();
+		}
+	}
+}
---
+++ FindMyCat/Services/PetService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FindMyCat.Services
+{
+	public class PetService : IPetService
+	{
+		public void DoNothing()
+		{
+			// do nothing
+		}
+	}
+}
